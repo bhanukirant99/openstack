af0ac41f27ba3a06982a0757a159e01248be4d41
Bhanu Kiran <bhanukirant99@gmail.com>
2023-04-21 04:55:53 +0530

    init

--- index.js
+++ index.js
@@ -1,34 +1,13 @@
 const list = [{
-    id: 1,
-    name: "test 1 Week Ago"
+    id: "55f4eWvcojEksyK9eOFKqLcKEcuPRUOr",
+    name: "test Since 1 Week Ago"
 }, {
-    id: 2,
-    name: "test1 1 Week Ago"
+    id: "365f25b2e17b4590a1ffa9a0d4be81f9",
+    name: "test1 Since 1 Week Ago"
 }, {
-    id: 3,
-    name: "test2 1 Week Ago"
-}, {
-    id: 4,
-    name: "test3 1 Week Ago"
-}, {
-    id: 5,
-    name: "test4 1 Weeks Ago"
-}, {
-    id: 6,
-    name: "VM 1 Month Ago"
-}, {
-    id: 7,
-    name: "VM 3 Months Ago"
-}, {
-    id: 8,
-    name: "VM 6 Months Ago"
-}, {
-    id: 9,
-    name: "VM 1 Year Ago"
-}, {
-    id: 10,
-    name: "VM More Than 1 Year Ago"
-}, ];
+    id: "365f25b2e17b45Wvcojafa9a0d590a1f",
+    name: "test2 Since 1 Week Ago"
+}];
 
 function displayVM() {
     const timeframe = document.getElementById("timeframe").value;
